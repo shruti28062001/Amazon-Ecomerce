5a010ab45ab99128ed85d8a3632e99e82e91fb7a
Shruti <shrutikadam7272@gmail.com>
2023-11-21 18:02:49 +0530

    first

---
+++ thumbnails.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#vertical').lightSlider({
+      gallery:true,
+      item:1,
+      vertical:true,
+      verticalHeight:295,
+      vThumbWidth:50,
+      thumbItem:8,
+      thumbMargin:4,
+      slideMargin:0
+    });  
+  });
+ 
